f505380eb335af99d6c88e008116fb5a1b497e39
ianictachos <tachos@ianic.gr>
2024-07-18 20:50:30 +0300

    changed file structure

--- src/i18n.js
+++ src/i18n.js
@@ -1,6 +1,6 @@
 // src/i18n.js
 import { createI18n } from "vue-i18n";
-import { el as elMessages, en as enMessages } from "./locale";
+import { el as elMessages, en as enMessages } from "../lib/locale";
 
 const messages = {
   el: {
