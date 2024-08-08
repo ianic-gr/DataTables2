37dcbd974a5af491c614fa0bec171500b755aff6
ianictachos <tachos@ianic.gr>
2024-08-08 15:42:43 +0300

    Removed a blueprint component warning

--- src/components/datatables/Blueprint.vue
+++ src/components/datatables/Blueprint.vue
@@ -16,40 +16,42 @@ provide("downloadModal", downloadModal);
 </script>
 
 <template>
-  <v-card>
-    <v-card-title>
-      <Header />
-    </v-card-title>
-    <v-card-text class="pa-0">
-      <component
-        ref="tableRef"
-        v-model="model"
-        :is="table_props.api ? TableServer : Table"
-      />
-    </v-card-text>
-  </v-card>
+  <div class="datatables-v2">
+    <v-card>
+      <v-card-title>
+        <Header />
+      </v-card-title>
+      <v-card-text class="pa-0">
+        <component
+          ref="tableRef"
+          v-model="model"
+          :is="table_props.api ? TableServer : Table"
+        />
+      </v-card-text>
+    </v-card>
 
-  <v-dialog v-model="downloadModal" max-width="350" persistent>
-    <v-list class="py-2" color="primary" elevation="12" rounded="lg">
-      <v-list-item
-        prepend-icon="mdi-file-download"
-        :title="$t('$datatables.preparing_download_file')"
-      >
-        <template v-slot:prepend>
-          <div class="pe-4">
-            <v-icon color="primary" size="x-large"></v-icon>
-          </div>
-        </template>
+    <v-dialog v-model="downloadModal" max-width="350" persistent>
+      <v-list class="py-2" color="primary" elevation="12" rounded="lg">
+        <v-list-item
+          prepend-icon="mdi-file-download"
+          :title="$t('$datatables.preparing_download_file')"
+        >
+          <template v-slot:prepend>
+            <div class="pe-4">
+              <v-icon color="primary" size="x-large"></v-icon>
+            </div>
+          </template>
 
-        <template v-slot:append>
-          <v-progress-circular
-            color="primary"
-            indeterminate="disable-shrink"
-            size="16"
-            width="2"
-          ></v-progress-circular>
-        </template>
-      </v-list-item>
-    </v-list>
-  </v-dialog>
+          <template v-slot:append>
+            <v-progress-circular
+              color="primary"
+              indeterminate="disable-shrink"
+              size="16"
+              width="2"
+            ></v-progress-circular>
+          </template>
+        </v-list-item>
+      </v-list>
+    </v-dialog>
+  </div>
 </template>
