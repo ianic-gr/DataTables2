38c519bf0aaab89f57666c9355e43ea3e600c1cd
ianictachos <tachos@ianic.gr>
2024-11-04 12:38:25 +0200

    Fix: Datepicker advanced filter component, multiple values formatting

--- src/components/datatables/header/AdvancedFilters/fields/components/Datepicker.vue
+++ src/components/datatables/header/AdvancedFilters/fields/components/Datepicker.vue
@@ -11,13 +11,32 @@ const props = defineProps({
 });
 
 const dateInputVal = computed({
-  get() {
-    return model.value
-      ? new Date(moment(model.value, props.returnFormat).format())
-      : null;
+  get: () => {
+    const format = props.returnFormat;
+    const isMultiple = Array.isArray(model.value);
+    let returnValue;
+
+    if (!model.value) return;
+
+    if (isMultiple && model.value.length) {
+      returnValue = model.value.map(
+        (val) => new Date(moment(val, format).format())
+      );
+    } else {
+      returnValue = new Date(moment(model.value, format).format());
+    }
+
+    return returnValue;
   },
-  set(v) {
-    model.value = moment(v).format(props.returnFormat);
+  set: (value) => {
+    const format = props.returnFormat;
+    const isMultiple = Array.isArray(value);
+
+    if (isMultiple && value.length) {
+      model.value = value.map((val) => moment(val).format(format));
+    } else {
+      model.value = moment(value).format(format);
+    }
   },
 });
 </script>
