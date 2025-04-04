f9a9a672f498c63be9baf404d956fed7bcb9f96b
ianictachos <tachos@ianic.gr>
2025-04-04 22:39:42 +0300

    Added Types

---
+++ lib/locale/index.d.ts
@@ -0,0 +1,14 @@
+declare module '@ianicdev/datatables2/locale' {
+    const el: {
+        [key: string]: any;
+    };
+    const en: {
+        [key: string]: any;
+    };
+    export { el, en };
+    const defaultExport: {
+        el: typeof el;
+        en: typeof en;
+    };
+    export default defaultExport;
+} 
