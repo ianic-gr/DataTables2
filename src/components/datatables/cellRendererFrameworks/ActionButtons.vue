92174b01259a81793ed09c47330a6996b2f3eae2
ianictachos <tachos@ianic.gr>
2025-05-15 11:30:18 +0300

    UI: Options list enhancement

--- src/components/datatables/cellRendererFrameworks/ActionButtons.vue
+++ src/components/datatables/cellRendererFrameworks/ActionButtons.vue
@@ -14,23 +14,36 @@ const items = computed(() => {
 </script>
 
 <template>
-  <v-menu class="action-buttons">
-    <template #activator="{ props: btnProps }">
-      <v-btn
-        icon="mdi-dots-vertical"
+  <v-btn icon="mdi-dots-vertical" variant="text" size="small">
+    <v-icon />
+    <v-menu
+      class="action-buttons"
+      location="bottom end"
+      activator="parent"
+      offset="4"
+    >
+      <v-list
+        bg-color="surface-light"
+        class="options-list d-flex flex-column ga-1 pa-1"
+        density="compact"
+        rounded="lg"
         variant="text"
-        v-bind="btnProps"
-        size="small"
-      />
-    </template>
-    <v-list>
-      <v-list-item v-for="(item, i) in items" v-bind="item" :key="i" />
-    </v-list>
-  </v-menu>
+        slim
+      >
+        <v-list-item
+          v-for="(item, i) in items"
+          v-bind="item"
+          :key="i"
+          rounded="lg"
+          link
+        />
+      </v-list>
+    </v-menu>
+  </v-btn>
 </template>
 
 <style lang="scss">
-.action-buttons {
+.options-list {
   .v-list-item__spacer {
     width: 10px !important;
   }
